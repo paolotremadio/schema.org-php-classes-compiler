1229bd0d26c1ea0e6f854c6e5a7b8496d81fdc5a
Paolo Tremadio <hello@paolotremadio.com>
2016-11-29 12:54:02 +0000

    Create data/ folder

--- makeDataDefs.php
+++ makeDataDefs.php
@@ -1,6 +1,6 @@
 <?php
 
-require dirname(__FILE__).'/schema.class.php';
+require dirname(__FILE__) . '/schema.class.php';
 /**
  * $json->properties
  * $json->types
@@ -16,42 +16,38 @@ $builder = new builder();
 $builder->fetchSchema();
 
 
-
 //die(var_dump($builder->schema->datatypes));
 
-foreach($builder->schema->types as $key => $type){
+foreach ($builder->schema->types as $key => $type) {
     //var_dump($key,$type);
 
     $className = '';
     $path = '';
 
-    foreach($type->ancestors as $extends){
-        $className .= $extends.'_';
-        $path .= $extends.'/';
+    foreach ($type->ancestors as $extends) {
+        $className .= $extends . '_';
+        $path .= $extends . '/';
     }
 
-   // $className = $className.$key;
+    $builder->startClass($className . $key, rtrim($className, "_"));
 
 
-    $builder->startClass($className.$key, rtrim($className, "_"));
-
-
-    foreach($type->properties as  $prop){
-
-        $builder->addClassProp($prop,$type->ancestors);
-       // die(var_dump($key,$prop));
+    foreach ($type->properties as $prop) {
+        $builder->addClassProp($prop, $type->ancestors);
     }
 
     $builder->endClass();
-    mkdir('data/'.$path, 0777, true);
-    file_put_contents("data/$path$key.php",$builder->data
+    if (!file_exists('data/' . $path)) {
+        mkdir('data/' . $path, 0777, true);
+    }
 
+    file_put_contents(
+        "data/$path$key.php", $builder->data
     );
+
     $builder->clearBuffer();
-   // die();
 }
 
-//var_dump(get_object_vars($json->types));
 
 
 
